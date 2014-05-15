479e2c9157022c933d99203dca1f68c965e0b6ee
Marko Nalis <chRonaldo@freenet.de>
2014-05-15 13:41:11 +0200

    klasse um die dateien automatisch zu laden und in windows explorer sichtbar zu machen

---
+++ src/com/example/swaluserstudy/MediaScannerHelp.java
@@ -0,0 +1,35 @@
+package com.example.swaluserstudy;
+
+import android.content.Context;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
+import android.util.Log;
+
+public class MediaScannerHelp implements MediaScannerConnectionClient {
+
+	private Context c;
+	
+	public MediaScannerHelp(Context context){
+		this.c = context;
+		
+	}
+	 public void addFile(String filename)
+	    {
+	        String [] paths = new String[1];
+	        paths[0] = filename;
+	        MediaScannerConnection.scanFile(c, paths, null, this);
+	    }
+	@Override
+	public void onMediaScannerConnected() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onScanCompleted(String path, Uri uri) {
+		 Log.i("ScannerHelper","Scan done - path:" + path + " uri:" + uri);
+
+	}
+
+}
